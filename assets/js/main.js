fe22c8e194cfcebcd33e53e4afab3b54bfea26f8
Jack Hanford <jackhanford@gmail.com>
2014-05-18 16:17:20 -0700

    Updated game, added cookie support for tracking high scores

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,57 +1,41 @@
 $(document).ready(function(){
-  var $btnGo = $('.bttn-success');
   var $btnStop = $('.bttn-danger');
-  var $btnRestart = $('.bttn-warning');
-
-  $btnGo.click(function(){
-    start();
-    difficulty();
-    $(this).addClass('bounceOutLeft').hide();
-    setTimeout(function(){$btnStop.addClass('bounceInRight').show()}, 1000);
-  });
+  var $btnRestart = $('.bttn-success');
 
   $btnStop.click(function(){
-    $btnRestart.removeClass('bounceOutLeft');
     shouldWeparty();
-    $btnGo.removeClass('bounceOutLeft');
-    $('#time').addClass('bounceIn').show();
-    $(this).addClass('bounceOutLeft').hide();
-    $btnRestart.addClass('bounceInRight').show();
+    $('#time').addClass('bounceIn').show()
     highscore();
   });
 
   $btnRestart.click(function(){
-    $('.celebrate').removeClass('lightSpeedIn').hide();
-    $('#time').removeClass('bounceIn');
-    $btnStop.removeClass('bounceOutLeft');
-    $btnStop.addClass('bounceInRight').show();
-    $(this).addClass('bounceOutLeft').hide();
+    $('#time').removeClass('bounceIn').show()
     start();
     difficulty();
   });
 
+  cookieGrab();
   show();
 });
 
 function shouldWeparty() {
   ms = parseFloat($('#time').text());
-  console.log(ms);
   if ( ms === 5000 ) {
-      console.log('yes');
-      $('.celebrate').addClass('lightSpeedIn').show();
-    } else {
-      if (ms > 4000 && ms < 5000) {
-        var dway = 5000 % ms;
-        $('.celebrate').text('Close! Within ' + dway + ' ms!').addClass('lightSpeedIn').show();
-      }
-      if (ms > 3000 && ms < 4000 || ms > 6000 && ms < 7000 ) {
-        $('.celebrate').text('Close! Within 2 seconds!').addClass('lightSpeedIn').show();
-      }
-      if (ms > 5000) {
-        var dway = ms % 5000;
-        $('.celebrate').text('Close! You went over ' + dway + ' ms!').addClass('lightSpeedIn').show();
-      }
+    console.log('yes');
+    $('.celebrate').addClass('lightSpeedIn').show();
+  } else {
+    if (ms > 4000 && ms < 5000) {
+      var dway = 5000 % ms;
+      $('.celebrate').text('Close! Within ' + dway + ' ms!').addClass('lightSpeedIn').show();
     }
+    if (ms > 3000 && ms < 4000 || ms > 6000 && ms < 7000 ) {
+      $('.celebrate').text('Close! Within 2 seconds!').addClass('lightSpeedIn').show();
+    }
+    if (ms > 5000) {
+      var dway = ms % 5000;
+      $('.celebrate').text('Close! You went over ' + dway + ' ms!').addClass('lightSpeedIn').show();
+    }
+  }
 }
 
 function difficulty() {
@@ -64,5 +48,26 @@ function difficulty() {
 
 function highscore() {
   var hs = 5000 % ms;
-  console.log(hs);
+  console.log(hs, ms);
+  if (hs < distance) {
+    $.cookie('score', ms);
+    $.cookie('distance_from_5', hs); 
+  }
+}
+
+function cookieGrab() {
+  $.cookie();
+
+  score = $.cookie('score');
+  distance = $.cookie('distance_from_5');
+
+  if (score === undefined) {
+    console.log('No cookie');
+    $('.highscore').hide();
+  } else {
+    $('.highscore').fadeIn();
+    $('.score').append(distance + 'ms');
+    console.log(score);
+    console.log(distance);
+  }
 }
